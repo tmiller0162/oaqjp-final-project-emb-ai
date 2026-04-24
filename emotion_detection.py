1f716735fa5379a627985a687e8f7c7ba3f800f1
Travis Miller <tmiller0162@gmail.com>
2026-04-24 15:52:18 -0500

    Add emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyze) {
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    api_get = requests.get(url=url, header=headers, input_json=input_json)
+    return api_get.text
