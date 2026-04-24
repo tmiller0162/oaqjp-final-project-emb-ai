432aeae66dc530c71b58ca4ef1479fbb6870b94d
Travis Miller <tmiller0162@gmail.com>
2026-04-24 17:31:26 -0500

    Push to theia

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,14 @@
 import requests
 
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    url = (
+            'https://sn-watson-emotion.labs.skills.network/'
+            'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+            )
+    header = {"grpc-metadata-mm-model-id":
+              "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     api_get = requests.post(url=url, headers=header, json=input_json)
-    return api_get.text #git test2
+    return api_get.text 
 
-print(emotion_detector("Testing the emotion detector, I am frustrated"))
+emotion_detector("I am very happy about this")
