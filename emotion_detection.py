0bfb8eed99c0b1b7c6bed47510eabcda71ab0416
Travis Miller <tmiller0162@gmail.com>
2026-04-24 16:17:18 -0500

    Testing thing

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,9 +2,9 @@ import requests
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-    api_get = requests.get(url=url, header=headers, input_json=input_json)
+    api_get = requests.get(url=url, headers=header, input_json=input_json)
     return api_get.text #git test2
 
 emotion_detector("Testing the emotion detector, I am frustrated")
