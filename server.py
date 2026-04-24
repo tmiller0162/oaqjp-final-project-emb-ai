b4cc8bd5d6347139960b189484fe8cfa05c72d00
Travis Miller <tmiller0162@gmail.com>
2026-04-24 18:23:00 -0500

    Push to theia

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import requests
 
 app = Flask("Emotion Detection")
 
+@app.route("/")
+def load_page():
+    return render_template("index.html")
+
 @app.route("/emotionDetector")
 def pass_input():
     text = requests.args.get("textToAnalyze")
