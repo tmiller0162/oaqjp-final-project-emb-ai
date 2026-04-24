35fe124f8a352063edca5d02b6d78d47112f885b
Travis Miller <tmiller0162@gmail.com>
2026-04-24 17:49:23 -0500

    Push to theia

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,15 +12,14 @@ def emotion_detector(text_to_analyze):
     api_get = requests.post(url=url, headers=header, json=input_json)
     raw_output = api_get.text
     emotions = json.loads(raw_output)['emotionPredictions'][0]['emotion']
-    #print(max(emotions, key=emotions.get))
-    print(emotions)
+    max_emotion = max(emotions, key=emotions.get)
     dict_return = {
-            'anger': anger_s,
-            'disgust': disgust_s,
-            'fear' : fear_s,
-            'joy': joy_s,
-            'sadness': sadness_s,
-            'dominant_emotion': 'thing'
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear' : emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': max_emotion
             }
 
 print(emotion_detector("I am very happy about this"))
